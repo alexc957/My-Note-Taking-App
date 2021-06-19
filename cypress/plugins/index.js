967122217bfdbe490f9570ca6c190b5070368436
alexc957 <alexcoronel1995@gmail.com>
2021-06-19 11:26:14 -0500

    adding the necesary packages to generate a report and upload it to covecov

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,5 +2,8 @@ const injectDevServer = require("@cypress/react/plugins/react-scripts")
 
 module.exports = (on, config) => {
   injectDevServer(on, config)
+  require('@cypress/code-coverage/task')(on, config)
+ 
+  
   return config
 }
